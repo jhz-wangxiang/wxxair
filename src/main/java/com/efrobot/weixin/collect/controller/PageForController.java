659b2ddd3cd22388a10a716d550063c70811f478
wangxiang <wangxiangxiang@ren001.com>
2018-02-11 15:41:31 +0800

    "air"

--- src/main/java/com/efrobot/weixin/collect/controller/PageForController.java
+++ src/main/java/com/efrobot/weixin/collect/controller/PageForController.java
@@ -1,7 +1,9 @@
 package com.efrobot.weixin.collect.controller;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,14 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.efrobot.weixin.baseapi.pojo.Area;
+import com.efrobot.weixin.baseapi.pojo.Channel;
 import com.efrobot.weixin.baseapi.pojo.FlightNum;
 import com.efrobot.weixin.baseapi.pojo.Order;
+import com.efrobot.weixin.baseapi.pojo.OrderStatusRecord;
 import com.efrobot.weixin.baseapi.pojo.User;
 import com.efrobot.weixin.collect.service.AreaService;
 import com.efrobot.weixin.collect.service.FlightNumService;
 import com.efrobot.weixin.collect.service.OrderService;
 import com.efrobot.weixin.collect.service.UserService;
 import com.efrobot.weixin.util.CommonUtil;
+import com.efrobot.weixin.util.SerialNum;
 import com.efrobot.weixin.util.WeixinUtil;
 
 @RequestMapping("/v1/page")
@@ -122,6 +128,134 @@ public class PageForController {
 			return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");//
 		}
 		return CommonUtil.resultMsg("SUCCESS", "校验成功");
+	}
+	@RequestMapping(value = "/getPrice", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> getPrice(Order record) throws Exception {
+		Map<String, Object> map = new HashMap<String, Object>();
+		// 价格计算
+		float c = 1;
+		float a = 1;
+		int p = 1;
+		if (null != record.getChannelId()) {// 渠道
+			Channel ch = new Channel();
+			ch.setId(record.getChannelId());
+			List<Channel> listch = orderService.getChannel(ch);
+			c = listch.get(0).getDiscount().longValue();
+		}
+		if (null != record.getAreaId()) {// 區域
+			Area ar = new Area();
+			ar.setId(record.getAreaId());
+			List<Area> listch = areaService.selectByParms(ar);
+			a = listch.get(0).getDiscount().longValue();
+			p = listch.get(0).getPrice();
+		}
+
+		int num = Integer.parseInt(record.getBaggageNum());
+		float paid = (float) ((p + (num - 1) * p * 0.1 * a) * 0.1 * c);
+		record.setTotalFee(new BigDecimal(num * p));
+		record.setPaidFee(new BigDecimal(paid));
+
+		map.put("resultCode", "SUCCESS");
+		map.put("ChannelDiscount", c);// 渠道折扣
+		map.put("AreaDiscount", a);// 区域折扣,第二件起的折扣
+		map.put("num", num);// 礼包数量
+		map.put("price", p);// 单间
+		map.put("totalFee", num * p);// 总额
+		map.put("paid", paid);// 实际支付价格
+		return map;
+	}
+	
+	@RequestMapping(value = "/insertOrder", method = RequestMethod.POST)
+	@ResponseBody
+	public Map<String, Object> insertOrder(Order record) throws Exception {
+		int result = -1;
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		SimpleDateFormat mmdd = new SimpleDateFormat("MMdd");
+		String datestr = sdf.format(new Date());
+		if ("".equals(record.getNowTimeStr()) || null == record.getNowTimeStr()) {
+			return CommonUtil.resultMsg("FAIL", "航班日期不能为空 ");
+		}
+		if ("".equals(record.getFlightNum()) || null == record.getFlightNum()) {
+			return CommonUtil.resultMsg("FAIL", "航班号不能为空 ");
+		}
+		FlightNum f = new FlightNum();
+		f.setFlightNum(record.getFlightNum());
+		List<FlightNum> list = flightNumService.selectByParms(f);
+		if (list.size() == 0) {
+			return CommonUtil.resultMsg("FAIL", "对不起，你航班的目的地还未开通此项服务。 ");
+		}
+
+		FlightNum flightNum = list.get(0);
+		if (record.getNowTime().getTime() <= flightNum.getEndTime().getTime()
+				&& record.getNowTime().getTime() >= flightNum.getStartTime().getTime()) {
+		} else {
+			return CommonUtil.resultMsg("FAIL", "对不起，你航班的目的地还未开通此项服务。 ");
+		}
+		// 校验航班时间
+		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyyMMdd HH:mm:ss");
+
+		String time = sdf.format(record.getNowTime()) + " " + flightNum.getEndHour() + ":00:00";
+
+		Date t = sdf2.parse(time);
+		Date n = new Date();
+		if (!CommonUtil.jisuan(n, t)) {
+			return CommonUtil.resultMsg("FAIL", "航班未开放，请于落地前 24 小时内下单");// 订单可在航班计划落地时间前
+																		// 24
+																		// 小时内下单，如早下单，则提示
+		}
+		String zm = flightNum.getExp1();
+		String orderNo = zm + datestr + mmdd.format(record.getNowTime()) + record.getFlightNum()
+				+ SerialNum.getSystemManageOrder();
+		// 异常处理
+		record.setOrderStatus(1);// 支付变成2
+		record.setPayStatus("未支付");
+		record.setCreateDate(new Date());
+		record.setOrderNo(orderNo);
+		record.setSingleWay("微信下单");
+		record.setAbnormalStatus("否");
+		// 价格计算
+		float c = 1;
+		float a = 1;
+		int p = 1;
+		if (null != record.getChannelId()) {// 渠道
+			Channel ch = new Channel();
+			ch.setId(record.getChannelId());
+			List<Channel> listch = orderService.getChannel(ch);
+			c = listch.get(0).getDiscount().longValue();
+		}
+		if (null != record.getAreaId()) {// 區域
+			Area ar = new Area();
+			ar.setId(record.getAreaId());
+			List<Area> listch = areaService.selectByParms(ar);
+			a = listch.get(0).getDiscount().longValue();
+			p = listch.get(0).getPrice();
+		}
+
+		int num = Integer.parseInt(record.getBaggageNum());
+		float paid = (float) ((p + (num - 1) * p * 0.1 * a) * 0.1 * c);
+		record.setTotalFee(new BigDecimal(num * p));
+		record.setPaidFee(new BigDecimal(paid));
+		result = orderService.insertSelective(record);
+		if (result == 0) {
+			return CommonUtil.resultMsg("FAIL", "未找到可编辑的信息");
+		} else if (result == 1) {
+			setHistory("提交订单", orderNo, "");
+			return CommonUtil.resultMsg("SUCCESS", "订单创建成功!");
+		} else {
+			return CommonUtil.resultMsg("FAIL", "更新异常: 多条数据被更新 ");
+		}
+	}
+	public int setHistory(String remark, String orderNo, String reason) {
+		OrderStatusRecord orderStatusRecord = new OrderStatusRecord();
+		orderStatusRecord.setRemark(remark);
+		orderStatusRecord.setExp1(orderNo);
+		orderStatusRecord.setExp2(reason);
+		orderStatusRecord.setCreateDate(new Date());
+		orderStatusRecord.setStatus(2);// 1-后台,2-客户
+		int result = orderService.insertSelective(orderStatusRecord);
+		return result;
+
 	}
 	@RequestMapping(value = "/orderDeal")
 	public String orderDeal(HttpServletRequest request) {
